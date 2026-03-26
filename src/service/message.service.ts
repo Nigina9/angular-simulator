192f4b0978be519ba613f3d8f04abc1cc2bf51b1
Nigina Zaripova <zaripovanigina1@gmail.com>
2026-03-26 12:02:44 +0300

    add message service

---
+++ src/service/message.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { IMessage } from '../interfaces/IMessage';
+import { Message } from '../enums/Message';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable()
+export class MessageService {
+
+  private messageSubject: BehaviorSubject<IMessage[]> = new BehaviorSubject<IMessage[]>([]);
+  messages$: Observable<IMessage[]> = this.messageSubject.asObservable();
+
+  private addMessage(type: Message, text: string): void {
+    const newMessage: IMessage = { type, text }
+    const currentMessage: IMessage[] = this.messageSubject.getValue()
+    const messages: IMessage[] = [newMessage, ...currentMessage];
+    this.messageSubject.next(messages)
+    setTimeout(() => this.closeMessage(newMessage), 5000);
+}
+
+  showWarn(text: string): void {
+    this.addMessage(Message.WARN, text);
+  }
+
+  showError(text: string): void {
+    this.addMessage(Message.ERROR, text);
+  }
+
+  showSuccess(text: string): void {
+    this.addMessage(Message.SUCCESS, text);
+  }
+
+  showInfo(text: string): void {
+    this.addMessage(Message.INFO, text);
+  }
+
+  closeMessage(message: IMessage): void {
+    const currentMessages: IMessage[] = this.messageSubject.getValue();
+    const updatedMessages: IMessage[] = currentMessages.filter(m => m !== message);
+    this.messageSubject.next(updatedMessages);
+  }
+
+}
