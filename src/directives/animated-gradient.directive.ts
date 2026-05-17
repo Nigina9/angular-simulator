4a87125d8e3c388513989585793fba5633aa300a
Nigina Zaripova <zaripovanigina1@gmail.com>
2026-05-17 14:23:49 +0300

    fix spaces

--- src/directives/animated-gradient.directive.ts
+++ src/directives/animated-gradient.directive.ts
@@ -13,13 +13,13 @@ export class AnimatedGradientDirective {
   constructor( private el: ElementRef, private render: Renderer2) {}
 
    @HostListener('mouseenter')
-    onMouseEnter() {
-      this.timerId = setTimeout(() => {
-        this.render.setStyle(this.el.nativeElement, 'transition', 'border 0.3s ease');
-        this.render.setStyle(this.el.nativeElement, 'border', `${this.gradientConfiguration.thickness || 2}px solid`);
-        const colors: string = this.gradientConfiguration.colors?.join(', ') || 'pink, purple';
-        this.render.setStyle(this.el.nativeElement, 'border-image', `linear-gradient(${colors}) 1`);
-      }, this.gradientConfiguration.delay || 1000);
+   onMouseEnter() {
+    this.timerId = setTimeout(() => {
+      this.render.setStyle(this.el.nativeElement, 'transition', 'border 0.3s ease');
+      this.render.setStyle(this.el.nativeElement, 'border', `${this.gradientConfiguration.thickness || 2}px solid`);
+      const colors: string = this.gradientConfiguration.colors?.join(', ') || 'pink, purple';
+      this.render.setStyle(this.el.nativeElement, 'border-image', `linear-gradient(${colors}) 1`);
+    }, this.gradientConfiguration.delay || 1000);
   }
 
   @HostListener('mouseleave')
