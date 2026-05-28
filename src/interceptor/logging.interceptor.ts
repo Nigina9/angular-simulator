1547e6e77e0ddc1d4fe9ed52c6fee0743fc17350
Nigina Zaripova <zaripovanigina1@gmail.com>
2026-05-28 20:15:16 +0300

    fixed(1)

--- src/interceptor/logging.interceptor.ts
+++ src/interceptor/logging.interceptor.ts
@@ -1,23 +1,21 @@
 import { HttpInterceptorFn, HttpRequest, HttpHandlerFn, HttpResponse, HttpErrorResponse} from '@angular/common/http';
-import { tap } from 'rxjs';
+import { catchError, tap, throwError } from 'rxjs';
 
 export const loggingInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
   const startTime: number = Date.now();
 
-  console.log(req.method, req.url);
-
   return next(req)
     .pipe(
-      tap({
-        next: (response): void => {
-          const requestTime: number = Date.now() - startTime;
-          if (response instanceof HttpResponse) {
-            console.log(`${ req.method } ${ req.url } - ${ response.status } - ${ requestTime }ms`);
-          }
-        },
-        error: (error: HttpErrorResponse): void => {
-          console.log(error);
+      tap((response) => {
+        const requestTime: number = Date.now() - startTime;
+        if (response instanceof HttpResponse) {
+          console.log(`Request success: ${ req.method } ${ req.url } - ${ response.status } - ${ requestTime }ms`);
         }
+      }),
+      catchError((error: HttpErrorResponse) => {
+        const requestTime: number = Date.now() - startTime;
+        console.log(`Request failed: ${ req.method } ${ req.url } - ${ error.status } - ${ requestTime }ms`);
+        return throwError(() => error);
       })
-    );
+    )
 };
